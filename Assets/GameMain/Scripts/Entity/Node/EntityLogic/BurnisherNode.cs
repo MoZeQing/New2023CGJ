0f4998d5f3856a2f85d37e6b88d9bae65e70d3cf
bulletrunner <2112094221@qq.com>
2023-07-09 09:26:08 +0800

    Update Burnisher
    更新研磨器

--- Assets/GameMain/Scripts/Entity/Node/EntityLogic/BurnisherNode.cs
+++ Assets/GameMain/Scripts/Entity/Node/EntityLogic/BurnisherNode.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using GameFramework.DataTable;
 using System.Collections;
 using System.Collections.Generic;
@@ -13,6 +14,17 @@ namespace GameMain
         private NodeData m_NodeData;
         private SpriteRenderer m_SpriteRenderer;
         private BoxCollider2D m_BoxCollider2D;
+
+        private BoxCollider2D m_BurnishBoxCollider2D;
+
+        private BaseCompenent m_AdsorbNode;
+        private bool m_Adsorb;
+
+        private Transform m_ProgressBar = null;
+        private float m_ProducingTime = 0f;
+
+
+
         protected override void OnInit(object userData)
         {
             base.OnInit(userData);
@@ -20,11 +32,23 @@ namespace GameMain
             m_NodeData = m_CompenentData.NodeData;
             GameEntry.Entity.AttachEntity(this.Id, m_CompenentData.OwnerId);
 
+            //获取到表
+            IDataTable<DRNode> dtNode = GameEntry.DataTable.GetDataTable<DRNode>();
+
+            DRNode drHotwaterNode = dtNode.GetDataRow(11);
+            m_NodeData.ProducingTime = drHotwaterNode.ProducingTime;
+            m_ProducingTime = m_NodeData.ProducingTime;
+
             m_SpriteRenderer = this.GetComponent<SpriteRenderer>();
             m_SpriteRenderer.sprite = GameEntry.Utils.nodeSprites[(int)m_NodeData.NodeTag];
 
             m_BoxCollider2D = this.GetComponent<BoxCollider2D>();
             m_BoxCollider2D.size = m_SpriteRenderer.size;
+
+            m_BurnishBoxCollider2D = this.transform.Find("BurnishBox").GetComponent<BoxCollider2D>();
+
+            m_ProgressBar = this.transform.Find("ProgressBar").transform;//获取进度条
+            m_ProgressBar.gameObject.SetActive(false);
 
         }
 
@@ -43,20 +67,82 @@ namespace GameMain
             if (Follow)
             {
                 this.transform.position = MouseToWorld(Input.mousePosition);
+                m_ProgressBar.gameObject.SetActive(false);
+                m_ProgressBar.transform.SetLocalScaleX(1);
+                m_ProducingTime = 0f;
+                m_AdsorbNode = null;
                 Producing = false;
-
             }
-            if (Completed)
+            if (m_AdsorbNode != null)
             {
-                Completed = false;
-                GameEntry.Entity.HideEntity(m_NodeData.Id);
+                Debug.Log("吸附中");
+                //吸附效果
+                //多个吸附点竞争时，寻找最近的吸附点吸附
+                if (m_AdsorbNode.Follow != false)
+                    return;
+                if (m_AdsorbNode.transform.parent.GetComponent<BaseNode>().NodeData.NodeTag != NodeTag.CoffeeBean)
+                    return;
 
+                m_AdsorbNode.ProducingTool = NodeTag.Burnisher;
+                m_AdsorbNode.Producing = true;
+                m_AdsorbNode.transform.DOMove(m_BurnishBoxCollider2D.transform.position, 0.1f);
+                Producing = true;
+                if (Producing)
+                {
+                    //处理进度条
+                    m_ProgressBar.gameObject.SetActive(true);
+                    m_ProgressBar.transform.SetLocalScaleX(1 - (1 - m_ProducingTime / m_NodeData.ProducingTime));
+                    m_ProducingTime -= Time.deltaTime;
+
+                    Debug.Log(m_ProducingTime);
+                    if (m_ProducingTime <= 0)
+                    {
+                        Producing = false;
+                        m_AdsorbNode.Producing = false;
+                        m_AdsorbNode.Completed = true;
+                        if (m_AdsorbNode.transform.parent.GetComponent<BaseNode>().NodeData.NodeTag == NodeTag.CoffeeBean)
+                        {
+                            GameEntry.Entity.ShowNode(new NodeData(GameEntry.Entity.GenerateSerialId(), 10000, NodeTag.GroundCoffee)
+                            {
+                                Position = this.transform.position
+                            });
+                        }
+                    }
+                }
             }
         }
 
         public void OnPointerDown(PointerEventData pointerEventData)
         {
             Follow = true;
+        }
+
+        public void OnTriggerEnter2D(Collider2D collision)
+        {
+            BaseCompenent baseCompenent = null;
+            if (collision.TryGetComponent<BaseCompenent>(out baseCompenent))
+            {
+                if (!baseCompenent.Follow)
+                    return;
+                if (!m_BurnishBoxCollider2D.IsTouching(baseCompenent.GetComponent<BoxCollider2D>()))
+                    return;
+                Debug.Log("检测到吸附");
+                m_AdsorbNode = baseCompenent;
+            }
+        }
+
+        public void OnTriggerExit2D(Collider2D collision)
+        {
+            BaseCompenent baseCompenent = null;
+            if (collision.TryGetComponent<BaseCompenent>(out baseCompenent))
+            {
+                if (!baseCompenent.Follow)
+                    return;
+                if (m_BurnishBoxCollider2D.IsTouching(baseCompenent.GetComponent<BoxCollider2D>()))
+                    return;
+                Debug.Log("检测到离开吸附");
+                m_AdsorbNode = null;
+            }
         }
 
         private Vector3 MouseToWorld(Vector3 mousePos)
