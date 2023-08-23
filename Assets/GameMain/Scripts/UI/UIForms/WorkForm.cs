3f8d15b9f7972bfa30b0ac3d9e9e9860278fe12c
MoZeQing <48617218+MoZeQing@users.noreply.github.com>
2023-08-23 18:22:33 +0800

    Update WorkStage
    1.更新WorkStage
    2.即将优化和重构代码结构

--- Assets/GameMain/Scripts/UI/UIForms/WorkForm.cs
+++ Assets/GameMain/Scripts/UI/UIForms/WorkForm.cs
@@ -12,13 +12,15 @@ namespace GameMain
 {
     public class WorkForm : UIFormLogic
     {
+        [SerializeField] private GameObject mBtnPrefab;
         [SerializeField] private Transform mCanvas;
         [Header("对话区域")]
-        [SerializeField] private Transform middleCanvas;
         [SerializeField] private Button dialogBtn;
         [SerializeField] private Text dialogText;
         [SerializeField] private Text nameText;
         [SerializeField] private Transform option;
+
+        private List<GameObject> m_Btns = new List<GameObject>();
         protected override void OnOpen(object userData)
         {
             base.OnOpen(userData);
@@ -41,7 +43,271 @@ namespace GameMain
                 nameText.text = string.Empty;
             }
         }
+        private void Option_Onclick(object sender, EventArgs e)
+        {
+            OptionData optionData = (OptionData)sender;
+            Next(optionData);
+        }
+        public void ShowButtons(List<OptionData> options)
+        {
+            ClearButtons();
+            foreach (OptionData option in options)
+            {
+                GameObject go = GameObject.Instantiate(mBtnPrefab, mCanvas);
+                go.GetComponent<OptionItem>().OnInit(option, Option_Onclick);
+                m_Btns.Add(go);
+            }
+        }
+        public void ClearButtons()
+        {
+            foreach (GameObject go in m_Btns)
+            {
+                Destroy(go);
+            }
+            m_Btns.Clear();
+        }
+        private int _index;
+        private DialogueGraph m_Dialogue = null;
+        private ChatTag chatTag;
+        private Node m_Node = null;
 
+        public void Next()
+        {
+            if (m_Node == null)
+                return;
+            switch (chatTag)
+            {
+                case ChatTag.Start:
+                    StartNode startNode = (StartNode)m_Node;
+                    Next(startNode);
+                    break;
+                case ChatTag.Chat:
+                    ChatNode chatNode = (ChatNode)m_Node;
+                    Next(chatNode);
+                    break;
+                case ChatTag.Option:
+                    OptionNode optionNode = (OptionNode)m_Node;
+                    break;
+                case ChatTag.Trigger:
+                    TriggerNode triggerNode = (TriggerNode)m_Node;
+                    Next(triggerNode);
+                    break;
+            }
+        }
+        private void Next(StartNode startNode)
+        {
+            if (startNode.GetOutputPort("start") != null)
+            {
+                var node = startNode.GetOutputPort("start").Connection.node;
+                switch (node.GetType().ToString())
+                {
+                    case "ChatNode":
+                        m_Node = node;
+                        chatTag = ChatTag.Chat;
+                        break;
+                    case "OptionNode":
+                        m_Node = node;
+                        chatTag = ChatTag.Option;
+                        break;
+                    case "TriggerNode":
+                        m_Node = node;
+                        chatTag = ChatTag.Trigger;
+                        break;
+                }
+                Next();
+            }
+        }
+        private void Next(ChatNode chatNode)
+        {
+            if (_index < chatNode.chatDatas.Count)
+            {
+                //角色控制
+                ChatData chatData = chatNode.chatDatas[_index];
+                nameText.text = chatData.charName;
+                dialogText.text = chatData.text;
+                if (chatData.actionData.actionTag != ActionTag.None)
+                {
+                    switch (chatData.actionData.actionTag)
+                    {
+                        case ActionTag.Jump:
+                            //跳动效果
+                            break;
+                        case ActionTag.Shake:
+                            //抖动效果
+                            break;
+                        case ActionTag.Squat:
+                            break;
+                    }
+                }
+                //角色控制
+                if (chatNode.GetPort(string.Format("chatDatas {0}", _index)) != null)
+                {
+                    NodePort nodePort = chatNode.GetPort(string.Format("chatDatas {0}", _index));
+                    if (nodePort.Connection != null)
+                    {
+                        Node node = nodePort.Connection.node;
+                        switch (node.GetType().ToString())
+                        {
+                            case "ChatNode":
+                                m_Node = node;
+                                chatTag = ChatTag.Chat;
+                                break;
+                            case "OptionNode":
+                                m_Node = node;
+                                chatTag = ChatTag.Option;
+                                break;
+                            case "TriggerNode":
+                                m_Node = node;
+                                chatTag = ChatTag.Trigger;
+                                break;
+                        }
+                        _index = 0;
+                    }
+                }
+                _index++;
+            }
+            else
+            {
+                //播放完毕
+                nameText.text = string.Empty;
+                dialogText.text = string.Empty;
+                _index = 0;
+                m_Dialogue = null;
+                m_Node = null;
+            }
+        }
+        private void Next(OptionData optionData)
+        {
+            if (optionData == null)
+                return;
+            ClearButtons();
+            OptionNode optionNode = (OptionNode)m_Node;
+            if (optionNode.GetPort(string.Format("optionDatas {0}", optionData.index)) != null)
+            {
+                NodePort nodePort = optionNode.GetPort(string.Format("optionDatas {0}", optionData.index));
+                if (nodePort.Connection != null)
+                {
+                    Node node = nodePort.Connection.node;
+                    switch (node.GetType().ToString())
+                    {
+                        case "ChatNode":
+                            m_Node = node;
+                            chatTag = ChatTag.Chat;
+                            break;
+                        case "OptionNode":
+                            m_Node = node;
+                            chatTag = ChatTag.Option;
+                            break;
+                        case "TriggerNode":
+                            m_Node = node;
+                            chatTag = ChatTag.Trigger;
+                            break;
+                    }
+                    _index = 0;
+                    Next();
+                }
+            }
+        }
+        private void Next(TriggerNode triggerNode)
+        {
+            if (triggerNode == null)
+                return;
+            string output = "b";
+            for (int i = 0; i < triggerNode.triggerDatas.Count; i++)
+            {
+                TriggerData data = triggerNode.triggerDatas[i];
+                if (GameEntry.Utils.Check(data.trigger))
+                {
+                    foreach (EventData eventData in data.events)
+                    {
+                        switch (eventData.eventTag)
+                        {
+                            case EventTag.Play:
+                                if (eventData.value == string.Empty)
+                                    output = string.Format("triggerDatas {0}", i);
+                                break;
+                            case EventTag.AddMoney:
+                                GameEntry.Utils.Money += int.Parse(eventData.value);
+                                break;
+                            case EventTag.AddFavor:
+                                GameEntry.Utils.Favor += int.Parse(eventData.value);
+                                break;
+                            case EventTag.AddFlag:
+                                GameEntry.Utils.AddFlag(eventData.value);
+                                break;
+                            case EventTag.RemoveFlag:
+                                GameEntry.Utils.RemoveFlag(eventData.value);
+                                break;
+                        }
+                    }
+                }
+            }
+            NextNode(triggerNode, output);
+        }
+        private void NextNode(TriggerNode node, string nodeName)
+        {
+            //如果没有中途跳转
+            if (node.GetPort(nodeName) != null)
+            {
+                NodePort nodePort = node.GetPort(nodeName);
+                if (nodePort.Connection != null)
+                {
+                    Node nextNode = nodePort.Connection.node;
+                    switch (nextNode.GetType().ToString())
+                    {
+                        case "ChatNode":
+                            m_Node = nextNode;
+                            chatTag = ChatTag.Chat;
+                            break;
+                        case "OptionNode":
+                            m_Node = nextNode;
+                            chatTag = ChatTag.Option;
+                            break;
+                        case "TriggerNode":
+                            m_Node = nextNode;
+                            chatTag = ChatTag.Trigger;
+                            break;
+                    }
+                    _index = 0;
+                    Next();
+                }
+            }
+        }
+        public void SetDialog(ChatNode chatNode)
+        {
+            _index = 0;
+            m_Node = chatNode;
+            chatTag = ChatTag.Chat;
+            Next();
+        }
+        public void SetDialog(DialogueGraph graph)
+        {
+            m_Dialogue = graph;
+            _index = 0;
+            foreach (Node node in m_Dialogue.nodes)
+            {
+                if (node.GetType().ToString() == "StartNode")
+                {
+                    m_Node = node;
+                    chatTag = ChatTag.Start;
+                }
+            }
+            Next();
+        }
+        public void SetDialog(string path)
+        {
+            m_Dialogue = (DialogueGraph)Resources.Load<DialogueGraph>(string.Format("DialogData/{0}", path));
+            _index = 0;
+            foreach (Node node in m_Dialogue.nodes)
+            {
+                if (node.GetType().ToString() == "StartNode")
+                {
+                    m_Node = node;
+                    chatTag = ChatTag.Start;
+                }
+            }
+            Next();
+        }
         private void GamePosEvent(object sender, GameEventArgs args)
         {
             GamePosEventArgs gamePos = (GamePosEventArgs)args;
