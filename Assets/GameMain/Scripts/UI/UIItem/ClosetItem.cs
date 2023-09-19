a9397bd838e9fcc89920a84ba94d918ca764943f
MoZeQing <48617218+MoZeQing@users.noreply.github.com>
2023-09-19 20:06:10 +0800

    Update ClosetForm
    1.更新了衣橱UI逻辑（仍有少量问题没有解决）

--- Assets/GameMain/Scripts/UI/UIItem/ClosetItem.cs
+++ Assets/GameMain/Scripts/UI/UIItem/ClosetItem.cs
@@ -8,43 +8,89 @@ using UnityEngine.UI;
 
 namespace GameMain
 {
-    public class ClosetItem : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    public class ClosetItem : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
     {
         [SerializeField] private Transform up;
         [SerializeField] private Transform down;
+        [SerializeField] private GameObject lockOn;
         [SerializeField] private Image img;
         [SerializeField] private ClosetForm closetForm;
-        [SerializeField] private ItemTag closetTag; 
+        [SerializeField] private ItemTag closetTag;
+
+
+        private ClosetItemState state;
 
         private void Start()
         {
             img.color = new Color(0.5f, 0.5f, 0.5f, 1f);
-        }
-
-        public void OnPointerEnter(PointerEventData pointerEventData)
-        {
-            this.transform.DOPause();
             if (GameEntry.Utils.GetPlayerItem(closetTag) == null ||
                 GameEntry.Utils.GetPlayerItem(closetTag).itemNum == 0)
             {
-                img.color = new Color(0.5f, 0.5f, 0.5f, 1f);
-                return;
+                lockOn.SetActive(true);
             }
             else
             {
                 closetForm.SetData(GameEntry.Utils.GetPlayerItem(closetTag));
+                lockOn.SetActive(false);
             }
-            this.transform.DOLocalMove(up.localPosition, 0.5f).SetEase(Ease.OutExpo);
-            img.color = Color.white;
+        }
+
+        public void SetState(ClosetItemState state)
+        {
+            this.state = state;
+        }
+
+        public void OnPointerEnter(PointerEventData pointerEventData)
+        {
+            if (state == ClosetItemState.Idle)
+            {
+                state = ClosetItemState.PickUp;
+                this.transform.DOPause();
+                if (GameEntry.Utils.GetPlayerItem(closetTag) == null ||
+                    GameEntry.Utils.GetPlayerItem(closetTag).itemNum == 0)
+                {
+                    img.color = new Color(0.5f, 0.5f, 0.5f, 1f);
+                    lockOn.SetActive(true);
+                    return;
+                }
+                else
+                {
+                    closetForm.SetData(GameEntry.Utils.GetPlayerItem(closetTag));
+                    lockOn.SetActive(false);
+                    img.color = Color.white;
+                }
+                this.transform.DOLocalMove(up.localPosition, 0.5f).SetEase(Ease.OutExpo);
+            }
         }
 
         public void OnPointerExit(PointerEventData pointerEventData)
         {
-            this.transform.DOPause();
-            this.transform.DOLocalMove(down.localPosition, 0.5f).SetEase(Ease.OutExpo);
-            closetForm.SetData(null);
-            img.color = new Color(0.5f, 0.5f, 0.5f, 1f);
+            if (state==ClosetItemState.PickUp)
+            {
+                state = ClosetItemState.Idle;
+                this.transform.DOPause();
+                this.transform.DOLocalMove(down.localPosition, 0.5f).SetEase(Ease.OutExpo);
+                closetForm.SetData(null);
+                img.color = new Color(0.5f, 0.5f, 0.5f, 1f);
+            }
+        }
+
+        public void OnPointerClick(PointerEventData pointerEventData)
+        {
+            if (state != ClosetItemState.Freeze)
+            {
+                state = ClosetItemState.Choice;
+                //·¢ËÍÏûÏ¢
+                closetForm.SetData(GameEntry.Utils.GetPlayerItem(closetTag), this);
+            }
         }
     }
 
+    public enum ClosetItemState
+    { 
+        Idle,
+        PickUp,
+        Choice,
+        Freeze,
+    }
 }
