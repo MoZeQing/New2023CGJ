6b181cc6ba1e143c9d4dd9f0487b50733edea3a1
bulletrunner <2112094221@qq.com>
2023-09-24 15:37:27 +0800

    Update
    更改了Item表里面的东西

---
+++ Assets/GameMain/Scripts/UI/UIItem/FoodItem.cs
@@ -0,0 +1,76 @@
+using GameMain;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class FoodItem : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] private Image FoodImg;
+    [SerializeField] private Text FoodText;
+    [SerializeField] private Text priceText;
+    [SerializeField] private Text warningPriceText;
+    [SerializeField] private Button okBtn;
+
+    private ShopItemData mShopItemData;
+    private Action<bool, ShopItemData> mTouchAction;
+    private Action mAction;
+
+    private void Start()
+    {
+        okBtn.onClick.AddListener(OnClick);
+    }
+    private void Update()
+    {
+
+        if (GameEntry.Utils.Money >= mShopItemData.price)
+        {
+            okBtn.interactable = true;
+            warningPriceText.gameObject.SetActive(false);
+        }
+        if (GameEntry.Utils.Money < mShopItemData.price)
+        {
+            okBtn.interactable = false;
+            warningPriceText.gameObject.SetActive(true);
+        }
+    }
+
+    public void SetData(ShopItemData shopItemData)
+    {
+        mShopItemData = shopItemData;
+        FoodText.text = shopItemData.itemName;
+        priceText.text = shopItemData.price.ToString();
+    }
+
+    private void OnClick()
+    {
+        if (GameEntry.Utils.Money >= mShopItemData.price)
+        {
+            mAction();
+            GameEntry.Utils.Money -= mShopItemData.price;
+            GameEntry.Dialog.PlayStory(mShopItemData.itemTag.ToString());
+        }
+    }
+
+    public void SetClick(Action action)
+    {
+        mAction = action;
+    }
+
+    public virtual void SetTouch(Action<bool, ShopItemData> action)
+    {
+        mTouchAction = action;
+    }
+
+    public virtual void OnPointerEnter(PointerEventData eventData)
+    {
+        mTouchAction(true, mShopItemData);
+    }
+
+    public virtual void OnPointerExit(PointerEventData eventData)
+    {
+        mTouchAction(false, mShopItemData);
+    }
+}
