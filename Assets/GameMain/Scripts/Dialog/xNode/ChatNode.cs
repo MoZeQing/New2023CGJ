fce79c949f7bf4dfe6c1c439146baee34b1480c5
MoZeQing <48617218+MoZeQing@users.noreply.github.com>
2023-09-27 00:23:26 +0800

    Update CSV
    1.更新转换CSV工具

--- Assets/GameMain/Scripts/Dialog/xNode/ChatNode.cs
+++ Assets/GameMain/Scripts/Dialog/xNode/ChatNode.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using XNode;
 using GameMain;
+using System.IO;
+using UnityEditor;
+using ExcelDataReader;
 
 [NodeWidth(400)]
 public class ChatNode : Node
@@ -44,6 +47,158 @@ public class ChatData
     public List<EventData> eventDatas;
     [TextArea,SerializeField]
     public string text;
+
+
+    [MenuItem("Data/Plot/Excel转换为剧情")]
+    public static void ExcelToPlot()
+    {
+        //string path = EditorUtility.OpenFilePanel("选择配置文件目录", Application.dataPath, " ");
+        //string info = new FileInfo(path).Name;//获取当前剧本名字
+        //FileStream stream = File.Open(path, FileMode.Open, FileAccess.Read);
+        //try
+        //{
+        //    var excelData = ExcelReaderFactory.CreateOpenXmlReader(stream);
+            
+        //    System.Data.DataSet sheets = excelData.AsDataSet();
+
+        //    foreach (System.Data.DataTable sheet in sheets.Tables)//对所有的表进行
+        //    {
+        //        string plotPath = "Assets/Resources/Data/PlotData/" + info;
+        //        //寻找当前存在的文件夹，如果不存在则创建
+        //        if (!Directory.Exists(plotPath))
+        //        {
+        //            Directory.CreateDirectory(plotPath);
+        //        }
+
+        //        var cols = sheet.Columns.Count;//ExcelFile的表Sheet的列数
+        //        var count = sheet.Rows.Count;//Sheet中的行数
+
+        //        for (int i = 0; i < cols; i += 3)
+        //        {
+        //            if (sheet.Rows[0][i].ToString() == "")//如果内容为空，跳出
+        //                break;
+        //            PlotScriptableObject plot = ScriptableObject.CreateInstance("PlotScriptableObject") as PlotScriptableObject;
+        //            PlotData plotData = new PlotData();
+        //            plot.plot = plotData;
+        //            plot.plot.plotTag = sheet.Rows[0][i].ToString();
+        //            string assetPath = string.Format("{0}/{1}.asset", plotPath, sheet.Rows[0][i].ToString());
+        //            for (int j = 2; j < count; j++)
+        //            {
+        //                if (sheet.Rows[j][i + 1].ToString() == "")//如果内容为空，跳出
+        //                    break;
+
+        //                DialogueData dialogue = new DialogueData();
+        //                if (sheet.Rows[j][i].ToString() == "")
+        //                    dialogue.charData = "char_null";
+        //                else
+        //                    dialogue.charData = sheet.Rows[j][i].ToString();//通过字符指定角色数据
+        //                dialogue.text = sheet.Rows[j][i + 1].ToString();
+        //                if (sheet.Rows[j][i + 2].ToString() != "")
+        //                {
+        //                    string[] events = sheet.Rows[j][i + 2].ToString().Split(' ', '=');//以'和=切分字符串
+        //                    for (int k = 0; k < events.Length; k += 2)
+        //                    {
+        //                        EventData data = new EventData
+        //                        {
+        //                            eventData = (EventEnum)Enum.Parse(typeof(EventEnum), events[k]),
+        //                            eventValue = events[k + 1]
+        //                        };
+        //                        dialogue.eventDatas.Add(data);
+        //                    }
+        //                }
+        //                plot.plot.dialogues.Add(dialogue);
+        //            }
+        //            AssetDatabase.CreateAsset(plot, assetPath);
+        //        }
+        //    }
+        //    stream.Close();
+        //    Debug.Log("转换完毕");
+        //}
+        //catch (Exception e)
+        //{
+        //    stream.Close();
+        //}
+    }
+
+    [MenuItem("Data/Plot/剧情转换为CSV")]
+    public static void PlotToCSV()
+    {
+        StorySO[] storySOs = Resources.LoadAll<StorySO>("StoryData");
+
+        //string path = EditorUtility.OpenFilePanel("选择配置文件目录", Application.dataPath, " ");
+        //string info = new FileInfo(path).Name;//获取当前剧本名字
+        //FileStream stream = File.Open(path, FileMode.Open, FileAccess.Read);
+        //try
+        //{
+        //    var excelData = ExcelReaderFactory.CreateOpenXmlReader(stream);
+        //    System.Data.DataSet sheets = excelData.AsDataSet();
+
+        //    foreach (System.Data.DataTable sheet in sheets.Tables)//对所有的表进行
+        //    {
+        //        string plotPath = "Assets/Resources/Data/PlotData/" + info;
+        //        //寻找当前存在的文件夹，如果不存在则创建
+        //        if (!Directory.Exists(plotPath))
+        //        {
+        //            Directory.CreateDirectory(plotPath);
+        //        }
+
+        //        var cols = sheet.Columns.Count;//ExcelFile的表Sheet的列数
+        //        var count = sheet.Rows.Count;//Sheet中的行数
+
+        //        for (int i = 0; i < cols; i += 3)
+        //        {
+        //            if (sheet.Rows[0][i].ToString() == "")//如果内容为空，跳出
+        //                break;
+        //            PlotScriptableObject plot = ScriptableObject.CreateInstance("PlotScriptableObject") as PlotScriptableObject;
+        //            PlotData plotData = new PlotData();
+        //            plot.plot = plotData;
+        //            plot.plot.plotTag = sheet.Rows[0][i].ToString();
+        //            string assetPath = string.Format("{0}/{1}.asset", plotPath, sheet.Rows[0][i].ToString());
+        //            for (int j = 2; j < count; j++)
+        //            {
+        //                if (sheet.Rows[j][i + 1].ToString() == "")//如果内容为空，跳出
+        //                    break;
+
+        //                DialogueData dialogue = new DialogueData();
+        //                if (sheet.Rows[j][i].ToString() == "")
+        //                    dialogue.charData = "char_null";
+        //                else
+        //                    dialogue.charData = sheet.Rows[j][i].ToString();//通过字符指定角色数据
+        //                dialogue.text = sheet.Rows[j][i + 1].ToString();
+        //                if (sheet.Rows[j][i + 2].ToString() != "")
+        //                {
+        //                    string[] events = sheet.Rows[j][i + 2].ToString().Split(' ', '=');//以'和=切分字符串
+        //                    for (int k = 0; k < events.Length; k += 2)
+        //                    {
+        //                        EventData data = new EventData
+        //                        {
+        //                            eventData = (EventEnum)Enum.Parse(typeof(EventEnum), events[k]),
+        //                            eventValue = events[k + 1]
+        //                        };
+        //                        dialogue.eventDatas.Add(data);
+        //                    }
+        //                }
+        //                plot.plot.dialogues.Add(dialogue);
+        //            }
+        //            AssetDatabase.CreateAsset(plot, assetPath);
+        //        }
+        //    }
+        //    stream.Close();
+        //    Debug.Log("转换完毕");
+        //}
+        //catch (Exception e)
+        //{
+        //    stream.Close();
+        //}
+    }
+}
+
+public class StoryCSVData
+{
+    public string storyName = "storyName";
+    public int foreDay=-1;
+    public int afterDay=-1;
+    public string plotTag="NULL";
 }
 [Serializable]
 public class CharData
