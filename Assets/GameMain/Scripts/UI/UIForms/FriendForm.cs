eaf58e6a42fe1d23873214ae2b51783c4dbd56fb
MoZeQing <48617218+MoZeQing@users.noreply.github.com>
2024-03-14 23:35:50 +0800

    Update Friend
    1.更新好友窗体
    2.更换了一部分的美术资源

--- Assets/GameMain/Scripts/UI/UIForms/FriendForm.cs
+++ Assets/GameMain/Scripts/UI/UIForms/FriendForm.cs
@@ -10,25 +10,94 @@ namespace GameMain
     {
         [SerializeField] private Button exitBtn;
 
-        public List<Image> images= new List<Image>();
-        public List<Text> texts= new List<Text>();
+        [SerializeField] private List<Image> images= new List<Image>();
+        [SerializeField] private List<Text> texts= new List<Text>();
+        [SerializeField] private Button leftBtn;
+        [SerializeField] private Button rightBtn;
+        [SerializeField] private Text text;
+
+        private List<KeyValuePair<string, int>> friends = new List<KeyValuePair<string, int>>();
+        private int index = 0;
         protected override void OnOpen(object userData)
         {
             base.OnOpen(userData);
+            index = 0;
+            leftBtn.interactable = false;
+            leftBtn.onClick.AddListener(Left);
+            rightBtn.onClick.AddListener(Right);
+
             exitBtn.onClick.AddListener(() => GameEntry.UI.CloseUIForm(this.UIForm));
-            int index = 0;
+            friends.Clear();
             foreach (KeyValuePair<string, int> pair in GameEntry.Utils.GetFriends())
             {
-                images[index].sprite = GameEntry.Utils.chars[pair.Key].sprite;
-                texts[index].text = string.Format("¹ØÏµ£º{0}",pair.Value.ToString());
+                friends.Add(pair);
+            }
+            for (int i = 0; i < images.Count; i++)
+            {
+                if (index >= friends.Count)
+                {
+                    index = 0;
+                    break;
+                }
+                images[i].sprite = GameEntry.Utils.chars[friends[index].Key].sprite;
+                texts[i].text = friends[index].Value.ToString(); ;
                 index++;
             }
+            if (GameEntry.Utils.GetFriends().Count <= images.Count)
+            {
+                rightBtn.interactable = false;
+            }
+            else
+            {
+                rightBtn.interactable = true;
+            }
+        }
+
+        private void Left()
+        {
+            rightBtn.interactable = true;
+            index -= images.Count*2;
+            if (index - images.Count < 0)
+                leftBtn.interactable = false;
+            for (int i = 0; i < images.Count; i++)
+            {
+                if (index >= friends.Count)
+                {
+                    index = 0;
+                    break;
+                }
+                images[i].sprite = GameEntry.Utils.chars[friends[index].Key].sprite;
+                texts[i].text = friends[index].Value.ToString(); ;
+                index++;
+            }
+            text.text = (index / images.Count).ToString();
+        }
+
+        private void Right()
+        {
+            leftBtn.interactable = true;
+            if (index + images.Count >= GameEntry.Utils.GetFriends().Count)
+                rightBtn.interactable = false;
+            for (int i = 0; i < images.Count; i++)
+            {
+                if (index >= friends.Count)
+                {
+                    index = 0;
+                    break;
+                }
+                images[i].sprite = GameEntry.Utils.chars[friends[index].Key].sprite;
+                texts[i].text = friends[index].Value.ToString(); ;
+                index++;
+            }
+            text.text = (index / images.Count).ToString();
         }
 
         protected override void OnClose(bool isShutdown, object userData)
         {
             base.OnClose(isShutdown, userData);
             exitBtn.onClick.RemoveAllListeners();
+            leftBtn.onClick.RemoveAllListeners();
+            rightBtn.onClick.RemoveAllListeners();
         }
     }
 }
