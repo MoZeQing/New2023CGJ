def1ffec3b071f0f016df7df30b5d97c7b92061c
MoZeQing <48617218+MoZeQing@users.noreply.github.com>
2024-03-26 17:05:29 +0800

    Update DialogData
    1.更新对话数据设置
    2.设置主场景和UI

--- Assets/GameMain/Scripts/UI/UIForms/MainForm.cs
+++ Assets/GameMain/Scripts/UI/UIForms/MainForm.cs
@@ -19,7 +19,6 @@ namespace GameMain
         [SerializeField] private Transform mCanvas;
         [SerializeField] private Animator mAnimator;
         [SerializeField] private TeachingForm mTeachingForm;
-        [SerializeField] private Transform canvas;
         [Header("Ö÷¿Ø")]
         [SerializeField] private Button loadBtn;
         [SerializeField] private Button saveBtn;
@@ -27,9 +26,8 @@ namespace GameMain
         [SerializeField] private Button guideBtn;
         [SerializeField] private Button friendBtn;
         [SerializeField] private Button recipeBtn;
-        [SerializeField] private Button closetBtn;
+        [SerializeField] private Button warehouseBtn;
         [SerializeField] private Button outBtn;
-        [SerializeField] private Button exitBtn;
         [SerializeField] private Button backBtn_1;
         [SerializeField] private Button backBtn_2;
         private PlaySoundParams playSoundParams = PlaySoundParams.Create();
@@ -47,9 +45,8 @@ namespace GameMain
             //guideBtn.onClick.AddListener(() => GameEntry.UI.OpenUIForm(UIFormId.GuideForm));
             friendBtn.onClick.AddListener(() => GameEntry.UI.OpenUIForm(UIFormId.FriendForm));
             recipeBtn.onClick.AddListener(() => GameEntry.UI.OpenUIForm(UIFormId.GuideForm));
-            closetBtn.onClick.AddListener(() => GameEntry.UI.OpenUIForm(UIFormId.ClosetForm));
-            outBtn.onClick.AddListener(() => canvas.gameObject.SetActive(true));
-            exitBtn.onClick.AddListener(() => canvas.gameObject.SetActive(false));
+            warehouseBtn.onClick.AddListener(() => Change(GamePos.Left));
+            outBtn.onClick.AddListener(() => Change(GamePos.Right));
             backBtn_1.onClick.AddListener(() => Change(GamePos.Up));
             backBtn_2.onClick.AddListener(() => Change(GamePos.Up));
             GameEntry.Event.Subscribe(MainFormEventArgs.EventId, OnMainFormEvent);
@@ -75,9 +72,8 @@ namespace GameMain
             //guideBtn.onClick.RemoveAllListeners();
             friendBtn.onClick.RemoveAllListeners();
             recipeBtn.onClick.RemoveAllListeners();
-            closetBtn.onClick.RemoveAllListeners();
+            warehouseBtn.onClick.RemoveAllListeners();
             outBtn.onClick.RemoveAllListeners();
-            exitBtn.onClick.RemoveAllListeners();
             backBtn_1.onClick.RemoveAllListeners();
             backBtn_2.onClick.RemoveAllListeners();
             GameEntry.Event.Unsubscribe(MainFormEventArgs.EventId, OnMainFormEvent);
