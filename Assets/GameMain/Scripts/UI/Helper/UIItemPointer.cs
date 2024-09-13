298540536b5cd9744cba146c292f0598e8aac2b4
MoZeQing <48617218+MoZeQing@users.noreply.github.com>
2024-09-14 00:59:53 +0800

    Update UIItemPointer
    1.更新UIItem动画器

--- Assets/GameMain/Scripts/UI/Helper/UIItemPointer.cs
+++ Assets/GameMain/Scripts/UI/Helper/UIItemPointer.cs
@@ -6,30 +6,33 @@ using DG.Tweening;
 using UnityEngine.UI;
 using GameMain;
 
-public class UIItemPointer : MonoBehaviour,IPointerEnterHandler,IPointerExitHandler
+public class UIItemPointer : MonoBehaviour,IPointerEnterHandler,IPointerExitHandler,IPointerDownHandler,IPointerUpHandler
 {
+    [SerializeField] private UIItemAction uiItemAction;
     [SerializeField] private UIItemTag uiItemTag;
-    [SerializeField] private Vector3 size = Vector3.one * 1.2f;
+    [SerializeField] private Vector3 initSize = Vector3.one * 1.2f;
+    [SerializeField] private Vector3 toSize = Vector3.one * 1.2f;
     [SerializeField] private float speed = 0.3f;
 
     [SerializeField] private Image image;
-    [SerializeField] private Animator animator;
     //“Ù–ß±‡∫≈
     [SerializeField] private int soundId;
 
     private void OnEnable()
     {
         this.transform.localScale = Vector3.one;
-        if(image!=null)
+        if(image!=null&&uiItemTag== UIItemTag.Holding)
             image.gameObject.SetActive(false);
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
+        if (uiItemAction != UIItemAction.Enter)
+            return;
         switch (uiItemTag)
         {
             case UIItemTag.Scaling:
-                this.transform.DOScale(size, speed);
+                image.transform.DOScale(toSize, speed);
                 break;
             case UIItemTag.Holding:
                 if (image != null)
@@ -44,10 +47,48 @@ public class UIItemPointer : MonoBehaviour,IPointerEnterHandler,IPointerExitHand
 
     public void OnPointerExit(PointerEventData eventData)
     {
+        if (uiItemAction != UIItemAction.Enter)
+            return;
         switch (uiItemTag)
         {
             case UIItemTag.Scaling:
-                this.transform.DOScale(Vector3.one, speed);
+                image.transform.DOScale(initSize, speed);
+                break;
+            case UIItemTag.Holding:
+                if (image != null)
+                    image.gameObject.SetActive(false);
+                break;
+        }
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (uiItemAction != UIItemAction.Click)
+            return;
+        switch (uiItemTag)
+        {
+            case UIItemTag.Scaling:
+                image.transform.DOScale(toSize, speed);
+                break;
+            case UIItemTag.Holding:
+                if (image != null)
+                    image.gameObject.SetActive(true);
+                break;
+        }
+        if (soundId != 0)
+        {
+            GameMain.GameEntry.Sound.PlaySound(soundId);
+        }
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if (uiItemAction != UIItemAction.Click)
+            return;
+        switch (uiItemTag)
+        {
+            case UIItemTag.Scaling:
+                image.transform.DOScale(initSize, speed);
                 break;
             case UIItemTag.Holding:
                 if (image != null)
@@ -56,10 +97,13 @@ public class UIItemPointer : MonoBehaviour,IPointerEnterHandler,IPointerExitHand
         }
     }
 }
-
+public enum UIItemAction
+{ 
+    Enter,
+    Click
+}
 public enum UIItemTag
 {
     Scaling,
-    Holding,
-    Animation,
+    Holding
 }
